b706000e128412e7320be594874f50119f10dfd0
developertarun01 <tarunusinessmail@gmail.com>
2024-12-06 17:00:45 +0530

    Backend

--- jan-seva-kendra-backend/index.js
+++ jan-seva-kendra-backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: "*", // Or specify your frontend domain
+    origin: "https://jan-seva-kendra-wheat.vercel.app/", // Or specify your frontend domain
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(bodyParser.json());
@@ -202,8 +202,11 @@ app.get('/api/services/image/:id', async (req, res) => {
     }
 });
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
